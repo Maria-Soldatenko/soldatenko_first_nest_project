795bfcb18b54fedb3abfb0c74366f9e4d8856925
maria <m.soldatenko@tg.dunice.net>
2023-12-27 09:32:39 +0300

    added-task-model

---
+++ db/models/task.entity.ts
@@ -0,0 +1,26 @@
+import {
+  Table,
+  Column,
+  Model,
+  CreatedAt,
+  UpdatedAt,
+  AllowNull,
+  Default,
+} from 'sequelize-typescript';
+
+@Table
+export class Task extends Model {
+  @AllowNull(false)
+  @Column
+  value: string;
+
+  @Default(false)
+  @Column
+  state: boolean;
+
+  @CreatedAt
+  creationDate: Date;
+
+  @UpdatedAt
+  updatedOn: Date;
+}
